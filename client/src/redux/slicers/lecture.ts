54a1011b4c9c1d8dda42b73c9d54bf52865c2b81
laca8 <ahmedragab01140331005@gmail.com>
2025-01-16 17:49:19 +0200

    last update

--- client/src/redux/slicers/lecture.ts
+++ client/src/redux/slicers/lecture.ts
@@ -1,13 +1,13 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import lectureService from "../services/lecture/lecture";
 import { objectId, lecture } from "../../types/type";
-type obj = lecture & objectId
+type obj = lecture & objectId;
 const initialState: {
-  game: lecture | null,
-  games: lecture[],
-  loading: boolean,
-  error: string | null,
-  success: boolean,
+  game: lecture | null;
+  games: lecture[];
+  loading: boolean;
+  error: string | null;
+  success: boolean;
 } = {
   game: null,
   games: [],
@@ -15,49 +15,73 @@ const initialState: {
   error: null,
   success: false,
 };
-export const addLecture = createAsyncThunk("addLecture", async (row:lecture, api) => {
-  try {
-    return await lectureService.createLecture(row);
-  } catch (error) {
-    // console.log(error);
-    return api.rejectWithValue((error as { response: { data: { message: string } } })?.response?.data?.message);
+export const addLecture = createAsyncThunk(
+  "addLecture",
+  async (row: lecture, api) => {
+    try {
+      return await lectureService.createLecture(row);
+    } catch (error) {
+      // console.log(error);
+      return api.rejectWithValue(
+        (error as { response: { data: { message: string } } })?.response?.data
+          ?.message
+      );
+    }
   }
-});
-export const fetchLectures = createAsyncThunk("fetchLectures", async (keyword:{code:string}, api) => {
+);
+export const fetchLectures = createAsyncThunk(
+  "fetchLectures",
+  async (keyword: { code: string; par: string }, api) => {
     try {
       const games = await lectureService.getLectures(keyword);
       return games as lecture[];
     } catch (error) {
       // console.log(error);
-      return api.rejectWithValue((error as { response: { data: { message: string } } })?.response?.data?.message);
-  
+      return api.rejectWithValue(
+        (error as { response: { data: { message: string } } })?.response?.data
+          ?.message
+      );
     }
-  });
+  }
+);
 export const fetchLecture = createAsyncThunk("fetchLecture", async (_, api) => {
   try {
     return await lectureService.getLecture();
   } catch (error) {
     // console.log(error);
-    return api.rejectWithValue((error as { response: { data: { message: string } } })?.response?.data?.message);
-
+    return api.rejectWithValue(
+      (error as { response: { data: { message: string } } })?.response?.data
+        ?.message
+    );
   }
 });
 
-export const removeLecture = createAsyncThunk("removeLecture", async (id:string, api) => {
-  try {
-    return await lectureService.deleteLecture(id);
-  } catch (error) {
-    return api.rejectWithValue((error as { response: { data: { message: string } } })?.response?.data?.message);
-  
+export const removeLecture = createAsyncThunk(
+  "removeLecture",
+  async (id: string, api) => {
+    try {
+      return await lectureService.deleteLecture(id);
+    } catch (error) {
+      return api.rejectWithValue(
+        (error as { response: { data: { message: string } } })?.response?.data
+          ?.message
+      );
+    }
   }
-});
-export const editLecture = createAsyncThunk("editLecture", async (row:obj, api) => {
-  try {
-    return await lectureService.updateLecture(row);
-  } catch (error) {
-    return api.rejectWithValue((error as { response: { data: { message: string } } })?.response?.data?.message);
+);
+export const editLecture = createAsyncThunk(
+  "editLecture",
+  async (row: obj, api) => {
+    try {
+      return await lectureService.updateLecture(row);
+    } catch (error) {
+      return api.rejectWithValue(
+        (error as { response: { data: { message: string } } })?.response?.data
+          ?.message
+      );
+    }
   }
-});
+);
 export const lectureSlice = createSlice({
   name: "lecture",
   initialState,
@@ -68,15 +92,15 @@ export const lectureSlice = createSlice({
         state.loading = true;
       })
       .addCase(addLecture.rejected, (state, action) => {
-            state.loading = false;
-            state.success = false;
-            state.error = action.payload as string;
-        })
+        state.loading = false;
+        state.success = false;
+        state.error = action.payload as string;
+      })
       .addCase(addLecture.fulfilled, (state, action) => {
         state.loading = false;
-          state.game = action.payload as lecture;
-          state.success = true;
-          state.error = null;
+        state.game = action.payload as lecture;
+        state.success = true;
+        state.error = null;
       })
       .addCase(fetchLecture.pending, (state) => {
         state.loading = true;
@@ -84,59 +108,55 @@ export const lectureSlice = createSlice({
       .addCase(fetchLecture.rejected, (state, action) => {
         state.success = false;
         state.loading = false;
-          state.error = action.payload as string;
-         
+        state.error = action.payload as string;
       })
       .addCase(fetchLecture.fulfilled, (state, action) => {
-        (state.loading = false);
-          (state.game = action.payload as lecture);
-          (state.success = true);
-          (state.error = null);
+        state.loading = false;
+        state.game = action.payload as lecture;
+        state.success = true;
+        state.error = null;
       })
       .addCase(fetchLectures.pending, (state) => {
         state.loading = true;
       })
       .addCase(fetchLectures.rejected, (state, action) => {
-        (state.success = false);
-          (state.loading = false);
-          state.error = action.payload as string;
-          
+        state.success = false;
+        state.loading = false;
+        state.error = action.payload as string;
       })
       .addCase(fetchLectures.fulfilled, (state, action) => {
-        (state.loading = false);
-          (state.games = action.payload);
-          (state.success = true);
-          (state.error = null);
+        state.loading = false;
+        state.games = action.payload;
+        state.success = true;
+        state.error = null;
       })
       .addCase(removeLecture.pending, (state) => {
         state.loading = true;
       })
       .addCase(removeLecture.rejected, (state, action) => {
-        (state.success = false);
-          (state.loading = false);
-          state.error = action.payload as string;
-          
+        state.success = false;
+        state.loading = false;
+        state.error = action.payload as string;
       })
       .addCase(removeLecture.fulfilled, (state, action) => {
-        (state.loading = false);
-          (state.game = action.payload);
-          (state.success = true);
-          (state.error = null);
+        state.loading = false;
+        state.game = action.payload;
+        state.success = true;
+        state.error = null;
       })
       .addCase(editLecture.pending, (state) => {
         state.loading = true;
       })
       .addCase(editLecture.rejected, (state, action) => {
-        (state.success = false);
-          (state.loading = false);
-          state.error = action.payload as string;
-          
+        state.success = false;
+        state.loading = false;
+        state.error = action.payload as string;
       })
       .addCase(editLecture.fulfilled, (state, action) => {
-        (state.loading = false);
-          (state.game = action.payload);
-          (state.success = true);
-          (state.error = null);
+        state.loading = false;
+        state.game = action.payload;
+        state.success = true;
+        state.error = null;
       });
   },
 });
